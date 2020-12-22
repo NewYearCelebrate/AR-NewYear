f96f0c5963a04ae05c92387098c06ac0c27fd4ee
CapSparkle <Feskovich.A@yandex.com>
2020-12-22 14:23:47 +0700

    Update threejs_worker_gltf.js

--- experience/snowman/threejs_worker_gltf.js
+++ experience/snowman/threejs_worker_gltf.js
@@ -33,7 +33,7 @@ var markers = {
     }
 };
 
-var setMatrix = function (matrix, value) {
+var setMatrix = function(matrix, value) {
     var array = [];
     for (var key in value) {
         array[key] = value[key];
@@ -45,7 +45,7 @@ var setMatrix = function (matrix, value) {
     }
 };
 
-function start( container, marker, video, input_width, input_height, canvas_draw, render_update, track_update) {
+function start(container, marker, video, input_width, input_height, canvas_draw, render_update, track_update) {
     var vw, vh;
     var sw, sh;
     var pscale, sscale;
@@ -72,7 +72,7 @@ function start( container, marker, video, input_width, input_height, canvas_draw
     //camera.matrixAutoUpdate = false;
     var camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
     camera.position.z = 15;
-	
+
     scene.add(camera);
 
     var light = new THREE.AmbientLight(0xffffff);
@@ -89,39 +89,40 @@ function start( container, marker, video, input_width, input_height, canvas_draw
     /* Load Model */
     var threeGLTFLoader = new THREE.GLTFLoader();
 
-    threeGLTFLoader.load("../Data/models/SnowMan.glb", function (gltf) {
-            model = gltf.scene.children[0];
-            model.position.z = 0;
-            model.position.x = 0;
-            model.position.y = 0;
-			model.scale.x = 200;
-			model.scale.y - 200;
-			model.scale.z = 200;
+    threeGLTFLoader.load("../Data/models/SnowMan.glb", function(gltf) {
+        model = gltf.scene.children[0];
+        model.position.z = 0;
+        model.position.x = 10;
+        model.position.y = 40.7;
+        model.scale.x *= 80;
+        model.scale.y *= 80;
+        model.scale.z *= 80;
 
-            var animation = gltf.animations[0];
-            var mixer = new THREE.AnimationMixer(model);
-            mixers.push(mixer);
-            var action = mixer.clipAction(animation);
-            action.play();
+        model.rotation.x -= 90;
 
-            root.matrixAutoUpdate = false;
-            root.add(model);
-        }
-    );
-	
-	const listener = new THREE.AudioListener();
-	camera.add( listener );
-	
-	const sound = new THREE.Audio( listener );
-	
-	const audioLoader = new THREE.AudioLoader();
-	audioLoader.load( '../Data/sound/audio_snowman.ogg', function( buffer ) {
-	sound.setBuffer( buffer );
-	sound.setLoop( true );
-	sound.setVolume( 0.5 );
-	
-});
-	
+        var animation = gltf.animations[0];
+        var mixer = new THREE.AnimationMixer(model);
+        mixers.push(mixer);
+        var action = mixer.clipAction(animation);
+        action.play();
+
+        root.matrixAutoUpdate = false;
+        root.add(model);
+    });
+
+    const listener = new THREE.AudioListener();
+    camera.add(listener);
+
+    const sound = new THREE.Audio(listener);
+
+    const audioLoader = new THREE.AudioLoader();
+    audioLoader.load('../Data/sound/audio_snowman.ogg', function(buffer) {
+        sound.setBuffer(buffer);
+        sound.setLoop(true);
+        sound.setVolume(0.5);
+
+    });
+
     var load = function() {
         vw = input_width;
         vh = input_height;
@@ -165,44 +166,48 @@ function start( container, marker, video, input_width, input_height, canvas_draw
         worker.onmessage = function(ev) {
             var msg = ev.data;
             switch (msg.type) {
-                case "loaded": {
-                    var proj = JSON.parse(msg.proj);
-                    var ratioW = pw / w;
-                    var ratioH = ph / h;
-                    proj[0] *= ratioW;
-                    proj[4] *= ratioW;
-                    proj[8] *= ratioW;
-                    proj[12] *= ratioW;
-                    proj[1] *= ratioH;
-                    proj[5] *= ratioH;
-                    proj[9] *= ratioH;
-                    proj[13] *= ratioH;
-                    //setMatrix(camera.projectionMatrix, proj);
-                    break;
-                }
-
-                case "endLoading": {
-                    if (msg.end == true) {
-                        // removing loader page if present
-                        var loader = document.getElementById('loading');
-                        if (loader) {
-                            loader.querySelector('.loading-text').innerText = 'Start the tracking!';
-                            setTimeout(function(){
-                                loader.parentElement.removeChild(loader);
-                            }, 2000);
-                        }
+                case "loaded":
+                    {
+                        var proj = JSON.parse(msg.proj);
+                        var ratioW = pw / w;
+                        var ratioH = ph / h;
+                        proj[0] *= ratioW;
+                        proj[4] *= ratioW;
+                        proj[8] *= ratioW;
+                        proj[12] *= ratioW;
+                        proj[1] *= ratioH;
+                        proj[5] *= ratioH;
+                        proj[9] *= ratioH;
+                        proj[13] *= ratioH;
+                        //setMatrix(camera.projectionMatrix, proj);
+                        break;
                     }
-                    break;
-                }
 
-                case "found": {
-                    found(msg);
-                    break;
-                }
-                case "not found": {
-                    found(null);
-                    break;
-                }
+                case "endLoading":
+                    {
+                        if (msg.end == true) {
+                            // removing loader page if present
+                            var loader = document.getElementById('loading');
+                            if (loader) {
+                                loader.querySelector('.loading-text').innerText = 'Start the tracking!';
+                                setTimeout(function() {
+                                    loader.parentElement.removeChild(loader);
+                                }, 2000);
+                            }
+                        }
+                        break;
+                    }
+
+                case "found":
+                    {
+                        found(msg);
+                        break;
+                    }
+                case "not found":
+                    {
+                        found(null);
+                        break;
+                    }
             }
             //track_update();
             process();
@@ -243,8 +248,8 @@ function start( container, marker, video, input_width, input_height, canvas_draw
             }
         }
     };
-	
-	var flagAudio = true;
+
+    var flagAudio = true;
 
     var draw = function() {
         //render_update();
@@ -257,10 +262,11 @@ function start( container, marker, video, input_width, input_height, canvas_draw
             root.visible = false;
         } else {
             root.visible = true;
-			if(flagAudio){
-				sound.play();
-			flagAudio = false;}
-				
+            if (flagAudio) {
+                sound.play();
+                flagAudio = false;
+            }
+
             // interpolate matrix
             for (var i = 0; i < 16; i++) {
                 trackedMatrix.delta[i] = world[i] - trackedMatrix.interpolated[i];
