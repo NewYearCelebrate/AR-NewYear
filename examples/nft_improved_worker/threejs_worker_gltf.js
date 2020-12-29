0c7f97bb038f07fee29e845874318626ea7ba5f9
CapSparkle <Feskovich.A@yandex.com>
2020-12-29 11:22:01 +0700

    Update threejs_worker_gltf.js

--- examples/nft_improved_worker/threejs_worker_gltf.js
+++ examples/nft_improved_worker/threejs_worker_gltf.js
@@ -6,7 +6,7 @@ function isMobile() {
     return /Android|mobile|iPad|iPhone/i.test(navigator.userAgent);
 }
 
-var interpolationFactor = 24;
+var interpolationFactor = 20;
 
 var trackedMatrix = {
     // for interpolation
